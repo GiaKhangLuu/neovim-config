c06bbf429e002cdc87ed87ee35bff298b9836879
GiaKhangLuu <waoknight@gmail.com>
2023-04-17 16:56:47 +0700

    first commit

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,8 @@
+vim.opt.laststatus = 2
+vim.opt.tabstop = 2  -- 1 tab = 2 space
+vim.opt.shiftwidth = 2 -- indent = 2 space
+vim.opt.autowrite = true
+vim.opt.autoread = true
+vim.opt.cursorline = true
+
+vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
